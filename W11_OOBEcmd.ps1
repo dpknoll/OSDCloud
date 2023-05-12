0152a7d3d9f7edf613ab6f1d78a702612fa8505e
dpknoll <dknoll@gmail.com>
2023-05-12 18:03:15 -0400

    Update W11_OOBEcmd.ps1

--- W11_OOBEcmd.ps1
+++ W11_OOBEcmd.ps1
@@ -80,7 +80,7 @@ If (!(Test-Path "C:\ProgramData\OSDeploy")) {
 }
 $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
 
-<#
+
 #================================================
 #  [PostOS] AutopilotOOBE Configuration Staging
 #================================================
@@ -113,13 +113,15 @@ $AutopilotOOBEJson = @"
     "Docs":  "https://google.com/",
     "Title":  "Autopilot Manual Register"
 }
+
+
 "@
 
 If (!(Test-Path "C:\ProgramData\OSDeploy")) {
     New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
 }
 $AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
-#>
+
 
 
 #================================================
