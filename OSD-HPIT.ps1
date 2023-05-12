9b57a022d4f81a6de495e1f7b2818acdac7c57e4
dpknoll <dknoll@gmail.com>
2023-05-12 15:23:45 -0400

    Update OSD-HPIT.ps1

--- OSD-HPIT.ps1
+++ OSD-HPIT.ps1
@@ -31,7 +31,7 @@ Import-Module OSD -Force
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
 $SetupCompleteCMD = @'
 powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
-powershell.exe -Command "& {IEX (IRM oobetasks.osdcloud.ch)}"
+powershell.exe -Command "& {IEX (IRM https://cleanup.osdcloud.ch)}"
 '@
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force   
 
