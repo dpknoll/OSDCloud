4dc823695ef2786dc35c646341bd374749b9e05e
dpknoll <dknoll@gmail.com>
2023-05-23 15:19:11 -0400

    test

--- ScriptPad/W10_OOBEcmd.ps1
+++ ScriptPad/W10_OOBEcmd.ps1
@@ -17,13 +17,14 @@ Import-Module OSD -Force
 #   [OS] Params and Start-OSDCloud
 #=======================================================================
 
-Write-Host -ForegroundColor Cyan "Set the Global Variables for a Driver Pack name --> none"
+<#Write-Host -ForegroundColor Cyan "Set the Global Variables for a Driver Pack name --> none"
 $Global:MyOSDCloud = @{
     DriverPackName = 'none'
     #ApplyManufacturerDrivers = $false
     #ApplyCatalogDrivers = $false
     #ApplyCatalogFirmware = $false
 }
+#>
 
 $Params = @{
     OSVersion = "Windows 10"
