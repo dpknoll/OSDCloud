4dc4d7474c837c4dae7bb82c052363b20caa7f54
dpknoll <dknoll@gmail.com>
2023-05-23 13:38:32 -0400

    Update W10_OOBEcmd.ps1

--- ScriptPad/W10_OOBEcmd.ps1
+++ ScriptPad/W10_OOBEcmd.ps1
@@ -35,6 +35,20 @@ $Params = @{
 }
 Start-OSDCloud @Params
 
+#================================================
+#  [PostOS] AutopilotOOBE CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBE.cmd"
+$OOBECMD = @'
+PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
+Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
+Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+Start /Wait PowerShell -NoL -C Start-OOBEDeploy
+REM Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/dpknoll/OSDCloud/9a5ab4df2700fa4d5875aa915307e683ca85d43e/CleanupOSDCloud
+Start /Wait PowerShell -NoL -C Restart-Computer -Force
+'@
+$OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Force
+
 #=======================================================================
 #   Restart-Computer
 #=======================================================================
