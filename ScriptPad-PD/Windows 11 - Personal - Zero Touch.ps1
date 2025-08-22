5d5847c4929d23eaef4d41c5d2e2c3c21e65fbcc
dpknoll <dknoll@gmail.com>
2025-08-22 08:30:45 -0400

    Add files via upload

---
+++ ScriptPad-PD/Windows 11 - Personal - Zero Touch.ps1
@@ -0,0 +1,65 @@
+#================================================
+#   [PreOS] Update Module
+#================================================
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force   
+
+#=======================================================================
+#   [OS] Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "24H2"
+    OSEdition = "Pro"
+    OSLanguage = "en-us"
+    OSLicense = "Retail"
+    ZTI = $true
+    Firmware = $false
+}
+Start-OSDCloud @Params
+
+#================================================
+#  [PostOS] AutopilotOOBE CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBE.cmd"
+$OOBECMD = @'
+PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
+Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
+Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+Start /Wait PowerShell -NoL -C Start-OOBEDeploy
+REM Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/dpknoll/OSDCloud/9a5ab4df2700fa4d5875aa915307e683ca85d43e/CleanupOSDCloud
+Start /Wait PowerShell -NoL -C Restart-Computer -Force
+'@
+$OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Force
+
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+REM  Moving OSDCloud Logs
+md C:\ProgramData\Microsoft\IntuneManagementExtension\OSD
+move /y C:\OSDCloud\Logs C:\ProgramData\Microsoft\IntuneManagementExtension\OSD
+move /y C:\ProgramData\OSDeploy C:\ProgramData\Microsoft\IntuneManagementExtension\OSD
+
+REM Cleanup directories
+rd /s /q C:\Drivers
+rd /s /q C:\OSDCloud
+rd /s /q C:\Temp
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
---
+++ ScriptPad-PD/Windows selection - OSDCloudGUI - TEST.ps1
@@ -0,0 +1,48 @@
+Write-Host  -ForegroundColor Cyan "Starting SeguraOSD's OSDCloud ..."
+Write-Host  -ForegroundColor Red "This option should only be used for testing. No support will be provided."
+Start-Sleep -Seconds 7
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+#Make sure I have the latest OSD Content
+<#Write-Host  -ForegroundColor Cyan "Updating the OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing the OSD PowerShell Module"
+Import-Module OSD -Force
+#>
+   
+       Write-Host  -ForegroundColor Cyan "Starting OSDCloudGUI"
+       Write-Host  -ForegroundColor Red "**WARNING** - Please make sure the correct driver pack is selected"
+       $OSDModuleResource.OSDCloud.Default.Edition = 'Pro'
+       $OSDModuleResource.OSDCloud.Default.Activation = 'Retail'
+       $OSDModuleResource.OSDCloud.Values.Name = 'Windows 11 23H2 x64','Windows 11 24H2 x64','Windows 10 22H2 x64'
+       $OSDModuleResource.StartOSDCloudGUI.BrandName = 'OSDCloud GUI'
+       $OSDModuleResource.StartOSDCloudGUI.BrandColor = 'RED'       
+       Start-Sleep -Seconds 10
+       Start-OSDCloudGUI
+       
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+REM  Moving OSDCloud Logs
+md C:\ProgramData\Microsoft\IntuneManagementExtension\OSD
+move /y C:\OSDCloud\Logs C:\ProgramData\Microsoft\IntuneManagementExtension\OSD
+move /y C:\ProgramData\OSDeploy C:\ProgramData\Microsoft\IntuneManagementExtension\OSD
+REM Cleanup directories
+rd /s /q C:\Drivers
+rd /s /q C:\OSDCloud
+rd /s /q C:\Temp
+'@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 10 seconds!"
+Start-Sleep -Seconds 10
+wpeutil reboot
